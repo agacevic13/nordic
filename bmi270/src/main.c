c8143c58610afc139983c77348a335b570be404a
Marija Sljukic <sljukic.marija22@gmail.com>
2024-12-04 11:47:52 +0100

    Triggers for acc and gyr, bmi160

--- bmi270/src/main.c
+++ bmi270/src/main.c
@@ -8,6 +8,48 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
 #include <stdio.h>
+static void print_bmi160_data(const struct device *bmi160)
+{
+	struct sensor_value acc[3];
+	struct sensor_value gyr[3];
+
+	if (sensor_channel_get(bmi160, SENSOR_CHAN_GYRO_XYZ, gyr) < 0) {
+		printf("Cannot read bmg160 gyro channels.\n");
+		return;
+	}
+	if (sensor_channel_get(bmi160, SENSOR_CHAN_GYRO_XYZ, acc) < 0) {
+		printf("Cannot read bmg160 acc channels.\n");
+		return;
+	}
+		sensor_channel_get(bmi160, SENSOR_CHAN_ACCEL_XYZ, acc);
+		sensor_channel_get(bmi160, SENSOR_CHAN_GYRO_XYZ, gyr);
+
+		printf("AX: %d.%06d; AY: %d.%06d; AZ: %d.%06d; "
+		       "GX: %d.%06d; GY: %d.%06d; GZ: %d.%06d;\n",
+		       acc[0].val1, acc[0].val2,
+		       acc[1].val1, acc[1].val2,
+		       acc[2].val1, acc[2].val2,
+		       gyr[0].val1, gyr[0].val2,
+		       gyr[1].val1, gyr[1].val2,
+		       gyr[2].val1, gyr[2].val2);
+}
+
+static void trigger_handler(const struct device *bmi160,
+			    const struct sensor_trigger *trigger)
+{
+	if (trigger->type != SENSOR_TRIG_DATA_READY &&
+	    trigger->type != SENSOR_TRIG_DELTA) {
+		printf("%p: trigger handler: unknown trigger type.\n", bmi160);
+		return;
+	}
+
+	if (sensor_sample_fetch(bmi160) < 0) {
+		printf("Gyro sample update error.\n");
+	}
+
+	print_bmi160_data(bmi160);
+}
+
 
 int main(void)
 {
@@ -64,24 +106,17 @@ int main(void)
 	sensor_attr_set(dev, SENSOR_CHAN_GYRO_XYZ,
 			SENSOR_ATTR_SAMPLING_FREQUENCY,
 			&sampling_freq);
+     struct sensor_trigger trig1, trig2;
+	 struct sensor_value attr;
 
-	while (1) {
-		/* 10ms period, 100Hz Sampling frequency */
-		k_sleep(K_MSEC(10));
+	trig1.type = SENSOR_TRIG_DELTA;
+	trig1.chan = SENSOR_CHAN_ACCEL_XYZ;
 
-		sensor_sample_fetch(dev);
-
-		sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, acc);
-		sensor_channel_get(dev, SENSOR_CHAN_GYRO_XYZ, gyr);
-
-		printf("AX: %d.%06d; AY: %d.%06d; AZ: %d.%06d; "
-		       "GX: %d.%06d; GY: %d.%06d; GZ: %d.%06d;\n",
-		       acc[0].val1, acc[0].val2,
-		       acc[1].val1, acc[1].val2,
-		       acc[2].val1, acc[2].val2,
-		       gyr[0].val1, gyr[0].val2,
-		       gyr[1].val1, gyr[1].val2,
-		       gyr[2].val1, gyr[2].val2);
-	}
+	trig2.type = SENSOR_TRIG_DELTA;
+	trig2.chan = SENSOR_CHAN_GYRO_XYZ;
+    sensor_trigger_set(dev, &trig1, trigger_handler);
+	sensor_trigger_set(dev, &trig2, trigger_handler);
+		
+	 
 	return 0;
 }
